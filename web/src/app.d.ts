16e7f7c6c9f8703ba44595192aec5395e39447fb
Isaac Hung <manhei5001@gmail.com>
2024-09-01 13:57:47 +0800

    reset web

--- web/src/app.d.ts
+++ web/src/app.d.ts
@@ -3,10 +3,7 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		interface Locals {
-			user: import('lucia').User | null;
-			session: import('lucia').Session | null;
-		}
+		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
--- web/src/lib/server/db/schema.ts
+++
@@ -1,8 +0,0 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
-
-export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	username: text('username').unique().notNull(),
-	password_hash: text('password_hash').notNull(),
-	email: text('email').notNull()
-});
